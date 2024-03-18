01174f1bc473834bce4017615bd0bb0bf0c1a586
mlinarik <40384547+mlinarik@users.noreply.github.com>
2024-03-17 21:46:58 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: false }));
 app.post('/login', (req, res) => {
  const username = req.body.username;
  const password = req.body.password;
 
+ if (!username || !password) {
+ console.log('invalid credentials');
+ return;
+ }
+
  // validate credentials here
- if (username && password) {
+ if (username === 'admin' && password === '4hs30jh58gs13n7ko') {
  console.log('valid credentials');
+ res.status(200).json({ success: true });
  } else {
  console.log('invalid credentials');
+ res.status(200).json({ success: false });
  }
 });
